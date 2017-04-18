bc6857fdb400f85c8ae508a66470e8175beb69ef
Achilleas Koutsou <achilleas.k@gmail.com>
2017-04-18 21:27:29 +0200

    New sample snippets

---
+++ snippets/nickex.py
@@ -0,0 +1,9 @@
+from aa import colorscale
+
+
+aa = colorscale(hue=3, brightness=4, contrast=2)
+
+
+print(aa.hue, 'is cool', sep=' ')
+print(aa.brightness, 'is bright', sep=' ')
+print(aa.contrast)
---
+++ snippets/printrand.py
@@ -0,0 +1,5 @@
+import random
+
+
+def print_rand(r=random.random()):
+    print("The value of r is {}".format(r))
